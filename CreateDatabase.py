8c08165fdd97f98e995bdbd659f4c9cf31a1464e
Berry <berryc3@wit.edu>
2020-07-17 16:41:18 -0400

    removed schedule columns from database tables

--- CreateDatabase.py
+++ CreateDatabase.py
@@ -8,7 +8,7 @@ c.execute('DROP TABLE IF EXISTS Instructor')
 c.execute('DROP TABLE IF EXISTS Admin')
 c.execute('DROP TABLE IF EXISTS Course')
 
-createStudentTable = """
+createStudentTable = '''
 CREATE TABLE Student (  
     ID 			INT 	    PRIMARY KEY NOT NULL,
     FirstName	TEXT	    NOT NULL,
@@ -16,13 +16,12 @@ CREATE TABLE Student (
     GradYear	INT 	    NOT NULL,
     Major		CHAR(4)     NOT NULL,
     Email		TEXT	    NOT NULL,
-    Password    TEXT        NOT NULL,
-    Schedule    TEXT
+    Password    TEXT        NOT NULL
 );
-"""
+'''
 c.execute(createStudentTable)
 
-createInstructorTable = """
+createInstructorTable = '''
 CREATE TABLE Instructor (
     ID          INT         PRIMARY KEY NOT NULL,
     FirstName   TEXT        NOT NULL,
@@ -30,13 +29,12 @@ CREATE TABLE Instructor (
     Title       TEXT        NOT NULL,
     Department  CHAR(4)     NOT NULL,
     Email       TEXT        NOT NULL,
-    Password    TEXT        NOT NULL,
-    Schedule    TEXT
+    Password    TEXT        NOT NULL
 );
-"""
+'''
 c.execute(createInstructorTable)
 
-createAdminTable = """
+createAdminTable = '''
 CREATE TABLE Admin (
     ID          INT         PRIMARY KEY NOT NULL,
     FirstName   TEXT        NOT NULL,
@@ -44,10 +42,10 @@ CREATE TABLE Admin (
     Email       TEXT        NOT NULL,
     Password    TEXT        NOT NULL
 );
-"""
+'''
 c.execute(createAdminTable)
 
-createCourseTable = """
+createCourseTable = '''
 CREATE TABLE Course (
     CRN         INT         PRIMARY KEY NOT NULL,
     Title       TEXT        NOT NULL,
@@ -57,69 +55,68 @@ CREATE TABLE Course (
     DaysOfWeek  TEXT        NOT NULL,
     Semester    TEXT        NOT NULL,
     Year        TEXT        NOT NULL,
-    Credits     TEXT        NOT NULL,
-    Roster      TEXT
+    Credits     TEXT        NOT NULL
 );
-"""
+'''
 c.execute(createCourseTable)
 
 #adding values to student table 
-c.execute("""INSERT INTO STUDENT VALUES(10001, 'Isaac', 'Newton', 1668, 'BSAS', 'newtoni', 'pass1', NULL);""") 
-c.execute("""INSERT INTO STUDENT VALUES(10002, 'Marie', 'Curie', 1903, 'BSAS', 'curiem', 'pass2', NULL);""") 
-c.execute("""INSERT INTO STUDENT VALUES(10003, 'Nikola', 'Tesla', 1878, 'BSEE', 'telsan', 'pass3', NULL);""") 
-c.execute("""INSERT INTO STUDENT VALUES(10004, 'Thomas', 'Edison', 1879, 'BSEE', 'notcool', 'pass4', NULL);""") 
-c.execute("""INSERT INTO STUDENT VALUES(10005, 'John', 'von Neumann', 1923, 'BSCO', 'vonneumannj', 'pass5', NULL);""") 
-c.execute("""INSERT INTO STUDENT VALUES(10006, 'Grace', 'Hopper', 1928, 'BCOS', 'hopperg', 'pass6', NULL);""") 
-c.execute("""INSERT INTO STUDENT VALUES(10007, 'Mae', 'Jemison', 1981, 'BSCH', 'jemisonm', 'pass7', NULL);""") 
-c.execute("""INSERT INTO STUDENT VALUES(10008, 'Mark', 'Dean', 1979, 'BSCO', 'deanm', 'pass8', NULL);""") 
-c.execute("""INSERT INTO STUDENT VALUES(10009, 'Michael', 'Faraday', 1812, 'BSAS', 'faradaym', 'pass9', NULL);""") 
-c.execute("""INSERT INTO STUDENT VALUES(10010, 'Ada', 'Lovelace', 1832, 'BCOS', 'lovelacea', 'pass10', NULL);""") 
+c.execute('''INSERT INTO STUDENT VALUES(10001, 'Isaac', 'Newton', 1668, 'BSAS', 'newtoni', 'pass1');''') 
+c.execute('''INSERT INTO STUDENT VALUES(10002, 'Marie', 'Curie', 1903, 'BSAS', 'curiem', 'pass2');''') 
+c.execute('''INSERT INTO STUDENT VALUES(10003, 'Nikola', 'Tesla', 1878, 'BSEE', 'telsan', 'pass3');''') 
+c.execute('''INSERT INTO STUDENT VALUES(10004, 'Thomas', 'Edison', 1879, 'BSEE', 'notcool', 'pass4');''') 
+c.execute('''INSERT INTO STUDENT VALUES(10005, 'John', 'von Neumann', 1923, 'BSCO', 'vonneumannj', 'pass5');''') 
+c.execute('''INSERT INTO STUDENT VALUES(10006, 'Grace', 'Hopper', 1928, 'BCOS', 'hopperg', 'pass6');''') 
+c.execute('''INSERT INTO STUDENT VALUES(10007, 'Mae', 'Jemison', 1981, 'BSCH', 'jemisonm', 'pass7');''') 
+c.execute('''INSERT INTO STUDENT VALUES(10008, 'Mark', 'Dean', 1979, 'BSCO', 'deanm', 'pass8');''') 
+c.execute('''INSERT INTO STUDENT VALUES(10009, 'Michael', 'Faraday', 1812, 'BSAS', 'faradaym', 'pass9');''') 
+c.execute('''INSERT INTO STUDENT VALUES(10010, 'Ada', 'Lovelace', 1832, 'BCOS', 'lovelacea', 'pass10');''') 
 
 #adding values to instructor table
-c.execute("""INSERT INTO INSTRUCTOR VALUES(20001, 'Joseph', 'Fourier', 'Full Prof.', 'BSEE', 'fourierj', 'word1', NULL);""") 
-c.execute("""INSERT INTO INSTRUCTOR VALUES(20002, 'Nelson', 'Mandela', 'Full Prof.', 'HUSS', 'mandelan', 'word2', NULL);""") 
-c.execute("""INSERT INTO INSTRUCTOR VALUES(20003, 'Galileo', 'Galilei', 'Full Prof.', 'BSAS', 'galileig', 'word3', NULL);""") 
-c.execute("""INSERT INTO INSTRUCTOR VALUES(20004, 'Alan', 'Turing', 'Associate Prof.', 'BSCO', 'turinga', 'word4', NULL);""") 
-c.execute("""INSERT INTO INSTRUCTOR VALUES(20005, 'Katie', 'Bouman', 'Assistant Prof.', 'BCOS', 'boumank', 'word5', NULL);""") 
-c.execute("""INSERT INTO INSTRUCTOR VALUES(20006, 'Daniel', 'Bernoulli', 'Associate Prof.', 'BSME', 'bernoullid', 'word6', NULL);""") 
+c.execute('''INSERT INTO INSTRUCTOR VALUES(20001, 'Joseph', 'Fourier', 'Full Prof.', 'BSEE', 'fourierj', 'word1');''') 
+c.execute('''INSERT INTO INSTRUCTOR VALUES(20002, 'Nelson', 'Mandela', 'Full Prof.', 'HUSS', 'mandelan', 'word2');''') 
+c.execute('''INSERT INTO INSTRUCTOR VALUES(20003, 'Galileo', 'Galilei', 'Full Prof.', 'BSAS', 'galileig', 'word3');''') 
+c.execute('''INSERT INTO INSTRUCTOR VALUES(20004, 'Alan', 'Turing', 'Associate Prof.', 'BSCO', 'turinga', 'word4');''') 
+c.execute('''INSERT INTO INSTRUCTOR VALUES(20005, 'Katie', 'Bouman', 'Assistant Prof.', 'BCOS', 'boumank', 'word5');''') 
+c.execute('''INSERT INTO INSTRUCTOR VALUES(20006, 'Daniel', 'Bernoulli', 'Associate Prof.', 'BSME', 'bernoullid', 'word6');''') 
 
 #adding value to admin table
-c.execute("""INSERT INTO ADMIN VALUES(30001, 'Malala', 'Yousafzai', 'yousafzaim', "password1");""") 
+c.execute('''INSERT INTO ADMIN VALUES(30001, 'Malala', 'Yousafzai', 'yousafzaim', 'password1');''') 
 
 #adding values to course table
-c.execute("""INSERT INTO COURSE VALUES(31799, 'Applied Programming Concepts','BSEE', 'Joseph Fourier', '1-2:50pm', 'TR', 'Summer', 2020, 4, NULL);""") 
-c.execute("""INSERT INTO COURSE VALUES(31406, 'Signals and Systems', 'HUSS', 'Nelson Mandela', '10-11:50am', 'MW', 'Summer', 2020, 4, NULL);""") 
-c.execute("""INSERT INTO COURSE VALUES(31290, 'Computer Architecture', 'BSAS', 'Galileo Galilei', '9:30-10:50am', 'TR', 'Summer', 2020, 4, NULL);""") 
-c.execute("""INSERT INTO COURSE VALUES(31047, 'Advanced Digital Circuit Design', 'BSCO', 'Alan Turing', '8-9:20am','WF', 'Summer', 2020, 4, NULL);""") 
-c.execute("""INSERT INTO COURSE VALUES(31044, 'Computer Networks', 'BCOS', 'Katie Bouman', '8-9:20am', 'TR', 'Summer', 2020, 4, NULL);""") 
+c.execute('''INSERT INTO COURSE VALUES(31799, 'Applied Programming Concepts','BSEE', 'Joseph Fourier', '1-2:50pm', 'TR', 'Summer', 2020, 4);''') 
+c.execute('''INSERT INTO COURSE VALUES(31406, 'Signals and Systems', 'HUSS', 'Nelson Mandela', '10-11:50am', 'MW', 'Summer', 2020, 4);''') 
+c.execute('''INSERT INTO COURSE VALUES(31290, 'Computer Architecture', 'BSAS', 'Galileo Galilei', '9:30-10:50am', 'TR', 'Summer', 2020, 4);''') 
+c.execute('''INSERT INTO COURSE VALUES(31047, 'Advanced Digital Circuit Design', 'BSCO', 'Alan Turing', '8-9:20am','WF', 'Summer', 2020, 4);''') 
+c.execute('''INSERT INTO COURSE VALUES(31044, 'Computer Networks', 'BCOS', 'Katie Bouman', '8-9:20am', 'TR', 'Summer', 2020, 4);''') 
 
 #printing student table
-print("Student table:")
-c.execute("""SELECT * FROM Student""")
+print('Student table:')
+c.execute('''SELECT * FROM Student''')
 query_result = c.fetchall()
   
 for i in query_result:
 	print(i)
 
 #printing instructor table
-print("Instructor table:")
-c.execute("""SELECT * FROM Instructor""")
+print('Instructor table:')
+c.execute('''SELECT * FROM Instructor''')
 query_result = c.fetchall()
   
 for i in query_result:
 	print(i)
 
 #printing admin table
-print("Admin table:")
-c.execute("""SELECT * FROM Admin""")
+print('Admin table:')
+c.execute('''SELECT * FROM Admin''')
 query_result = c.fetchall()
   
 for i in query_result:
 	print(i)
 
 #printing course table    
-print("Course table:")
-c.execute("""SELECT * FROM Course""")
+print('Course table:')
+c.execute('''SELECT * FROM Course''')
 query_result = c.fetchall()
   
 for i in query_result:
